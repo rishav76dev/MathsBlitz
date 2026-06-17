0eb707b456631612c97a959a28934d36bbcc64a0
rishav76dev <rjrishav037@gmail.com>
2026-06-17 17:17:51 +0530

    add talent verfication tag

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
+      <head>
+        <meta name="talentapp:project_verification" content="d9ed064beb03ecab65fa06938976bd3b94f33e8e8c7f4b2fc0b8c10f8234f4becaa097aa22bfd141250fad3a79ac274d957c34fb6100680c51db686bf334c6f9" />
+      </head>
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
       </body>
