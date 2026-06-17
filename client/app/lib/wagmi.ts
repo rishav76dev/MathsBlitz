f168dbcfc78ca30b421f61f455c4b4023ae5f494
rishav76dev <rjrishav037@gmail.com>
2026-06-17 18:24:06 +0530

    update wagmin config to mainnet

--- client/app/lib/wagmi.ts
+++ client/app/lib/wagmi.ts
@@ -1,20 +1,20 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { celoSepolia } from "viem/chains";
+import { celo } from "viem/chains";
 import { http } from "wagmi";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "MathsBlitz",
   projectId:
     process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "mathsblitzdemo000000000000000000",
-  chains: [celoSepolia],
+  chains: [celo],
   transports: {
-    [celoSepolia.id]: http("https://forno.celo-sepolia.celo-testnet.org"),
+    [celo.id]: http("https://forno.celo.org"),
   },
   ssr: true,
 });
 
-export const CELO_CHAIN = celoSepolia;
-export const CELO_CHAIN_ID = celoSepolia.id;
+export const CELO_CHAIN = celo;
+export const CELO_CHAIN_ID = celo.id;
 
 declare module "wagmi" {
   interface Register {
